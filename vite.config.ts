6ae0ddc3ed0bc7a810b8c7d93a56eca185d3ef11
Sean Wong <seanwong24@live.com>
2024-05-16 20:37:03 -0700

    set up PWA basis

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "PWA Notepad",
+        short_name: "PWANotepad",
+        start_url: "/app",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "./icon512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["@hey-web-components/monaco-editor"],
   },
