05c5c4dee5c1bd282537658f5e5a9d99b58ba6fa
Sean Wong <seanwong24@live.com>
2024-06-13 13:48:31 -0700

    tweak light theme

--- src/utils/modified-fluent-light-theme.ts
+++ src/utils/modified-fluent-light-theme.ts
@@ -2,5 +2,5 @@ import { webLightTheme } from "@fluentui/react-components";
 
 export const modifiedFluentLightTheme = {
   ...webLightTheme,
-  colorNeutralBackground1: "#dbdbdb",
+  colorNeutralBackground1: "#f0f0f0",
 };
